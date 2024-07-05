d0cc7016d7c4a09a45246980dcde28243abd6c95
Roberto Serra <roberto.serra@waterview.it>
2024-07-05 18:03:41 +0200

    path fixed

--- chat.sh
+++ chat.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-source .env
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+source "$SCRIPT_DIR/.env"
 
 $PYTHON_INTERPRETER -m llm_client
--- llm_client/main.py
+++ llm_client/main.py
@@ -1,7 +1,9 @@
+import re
 import sys
 import requests
 
 from llm_client.logger import Logger
+import subprocess
 
 
 
@@ -9,6 +11,18 @@ from llm_client.logger import Logger
 CHAT_URL = " http://localhost:11434/api/chat"
 MODEL = "mistral"
 
+
+# assistant_system_prompt = """You are an helpful AI assistant named Bernard.
+# If you are asked to search something, your response should have the following format without any deviation or other words:
+# EXECUTE_COMMAND: `<search_query>`
+# where <search_query> is a valid bash grep command for a Ubuntu system.
+# For example, if you are asked to search for the word "hello" in the file "file.txt", your response should be:
+# EXECUTE_COMMAND: `grep hello file.txt`
+# Answer normally if you are not asked to search for something.
+# """
+
+
+assistant_system_prompt = """You are an helpful AI assistant named Bernard."""
 
 
 def run_chat():
@@ -18,7 +32,7 @@ def run_chat():
 
     print("Hello from llm_client!")
 
-    messages = []
+    messages = [{"role": "system", "content": assistant_system_prompt}]
 
     try:
         while True:
@@ -33,7 +47,15 @@ def run_chat():
             response = r.json()
             logger.info(response)
             message = response["message"]
-            print(f"{MODEL}: {message["content"]}")
+            content = message["content"]
+            # if content.strip().startswith("EXECUTE_COMMAND:"):
+            #     match = re.search(r'`.*`', content)
+            #     if match:
+            #         cmd = match.group(0).replace("`", "")
+            #         print("COMMAND:", cmd)
+            #         process = subprocess.Popen(cmd.split(" "), stdout=subprocess.PIPE)
+            #         print(process.stdout.read())
+            print(f"{MODEL}: {content}")
 
             messages.append(message)
             
