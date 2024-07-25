8475e7144ca79ac20503f6a64675979b5b7aecca
Roberto Serra <roberto.serra@waterview.it>
2024-07-25 16:58:40 +0200

    ask-many prompt from pipe

--- scripts/ask-many.sh
+++ scripts/ask-many.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-
-if [ -z "$1" ]; then
-    PROMPT=$(cat prompt.txt)
+if [ -p /dev/stdin ]; then
+    PROMPT=$(cat -)
+elif [ -z "$1" ]; then
+    PROMPT=$(cat ./prompts/prompt.txt)
 else
     PROMPT="$@"
 fi
 
-jq -r '.[]' models.json | while read i; do
+echo  "[+] Prompt: $PROMPT"
+
+jq -r '.[]' ./models.json | while read i; do
     echo "[+] Model: $i"
     echo ""
 
     json_data='{"model": "'$i'", "prompt": "'$PROMPT'", "stream": false}'
-    # echo $JSON_DATA
+    # echo $json_data
     full_response=$(curl --silent http://localhost:11434/api/generate -d "$json_data")
     assistant_resp=$(echo $full_response | jq '.response')
     echo $assistant_resp
